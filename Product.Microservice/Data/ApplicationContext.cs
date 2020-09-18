3cdb46a344a54144d82710337a15e99f462e8930
Amila Fernando <amilafernand@gmail.com>
2020-09-18 19:18:17 +0530

    intitial commit

---
+++ Product.Microservice/Data/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Product.Microservice.Data
+{
+    public class ApplicationContext : DbContext, IApplicationContext
+    {
+        public DbSet<Entities.Product> Products { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        { 
+
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
